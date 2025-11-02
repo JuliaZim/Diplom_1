66dd0057572c43e301d1ed050b225327b9b12e91
Julia Zimina <krypina2009@yandex.ru>
2025-11-02 14:39:05 +0300

    Добавлены моки в тесты test_burger и test_database

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,5 +1,7 @@
 from praktikum.burger import Burger
 from data import data
+from unittest.mock import Mock 
+
 
 
 
@@ -9,38 +11,65 @@ class TestBurger:
         assert burger.bun == None
         assert burger.ingredients == []
 
-    def test_set_buns_to_burger(self, burger, bun):
-        burger.set_buns(bun)
+    def test_set_buns_to_burger(self, burger):
+        mock_bun = Mock()
+        mock_bun.configure_mock(name=data.bun_name, price = data.bun_price)
+        burger.set_buns(mock_bun)
         assert burger.bun.name == data.bun_name
 
-    def test_add_ingredient_one_ingredient(self, burger, ingredient):
-        burger.add_ingredient(ingredient)
+    def test_add_ingredient_one_ingredient(self, burger):
+        mock_ingredient = Mock()
+        mock_ingredient.configure_mock(type = data.ingredient_type,name=data.ingredient_name, price = data.ingredient_price)
+        burger.add_ingredient(mock_ingredient)
         assert burger.ingredients[0].name == data.ingredient_name
         assert burger.ingredients[0].type == data.ingredient_type
         assert burger.ingredients[0].price == data.ingredient_price
 
-    def test_remove_ingredient(self, burger, ingredient):
-        burger.add_ingredient(ingredient)
+    def test_remove_ingredient(self, burger):
+        mock_ingredient = Mock()
+        mock_ingredient.configure_mock(type = data.ingredient_type,name=data.ingredient_name, price = data.ingredient_price)
+        burger.add_ingredient(mock_ingredient)
         assert burger.ingredients[0].name == data.ingredient_name
         burger.remove_ingredient(0)
         assert burger.ingredients == []
 
     def test_move_ingredient(self, burger, ingredient):
-        burger.add_ingredient(ingredient)
-        burger.add_ingredient(ingredient)
+        mock_ingredient = Mock()
+        mock_ingredient.configure_mock(type = data.ingredient_type,name=data.ingredient_name, price = data.ingredient_price)
+        burger.add_ingredient(mock_ingredient)
+        burger.add_ingredient(mock_ingredient)
         burger.move_ingredient(0, 1)
         assert burger.ingredients[0].name == data.ingredient_name
         assert burger.ingredients[1].name == data.ingredient_name
 
-    def test_get_price_burger(self, burger, ingredient, bun):
-        burger.set_buns(bun)
-        burger.add_ingredient(ingredient)
+    def test_get_price_burger(self, burger):
+        mock_ingredient = Mock()
+        mock_ingredient.configure_mock(name=data.ingredient_name, type=data.ingredient_type, price=data.ingredient_price)
+        mock_ingredient.get_price = Mock(return_value=data.ingredient_price) 
+
+        mock_bun = Mock()
+        mock_bun.configure_mock(price=data.bun_price, name=data.bun_name)
+        mock_bun.get_price = Mock(return_value=data.bun_price) 
+
+        burger.set_buns(mock_bun)
+        burger.add_ingredient(mock_ingredient)
         price = burger.bun.price * 2 + burger.ingredients[0].price
         assert price == burger.get_price()
 
     def test_get_receipt_burger(self, burger, ingredient, bun):
-        burger.set_buns(bun)
-        burger.add_ingredient(ingredient)
+        mock_ingredient = Mock()
+        mock_ingredient.configure_mock(name=data.ingredient_name, type=data.ingredient_type, price=data.ingredient_price)
+        mock_ingredient.get_price = Mock(return_value=data.ingredient_price) 
+        mock_ingredient.get_name = Mock(return_value=data.ingredient_name)
+        mock_ingredient.get_type = Mock(return_value=data.ingredient_type)
+
+        mock_bun = Mock()
+        mock_bun.configure_mock(price=data.bun_price, name=data.bun_name)
+        mock_bun.get_price = Mock(return_value=data.bun_price) 
+        mock_bun.get_name = Mock(return_value=data.bun_name)
+
+        burger.set_buns(mock_bun)
+        burger.add_ingredient(mock_ingredient)
 
         price = burger.bun.price * 2 + burger.ingredients[0].price
         assert (
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,34 +1,53 @@
 import pytest
 from praktikum.database import Database
 from data import data
+from unittest.mock import Mock 
 
 
 class TestDatabase:
     def test_create_bun_in_database(self, database):
+        mock_bun = Mock()
+        mock_bun.name = data.bun_name
+        mock_bun.price = data.bun_price
+        database.buns = [mock_bun]
         bun_list = [[bun.name, bun.price] for bun in database.buns]
-        assert bun_list == data.bun_list
+        assert bun_list == [[data.bun_name,data.bun_price]]
 
     @pytest.mark.parametrize(
         "ingredient_type, ingredient_value",
         [("SAUCE", data.sauce_names_in_dtb), ("FILLING", data.filling_names_in_dtb)],
     )
     def test_create_ingredient_in_database(self, database, ingredient_type, ingredient_value):
-        database = Database()
-        ingredient = list(
-            filter(lambda x: x.type == ingredient_type, database.ingredients)
-        )
+        mock_ingredients = []
+        for value in ingredient_value:
+            mock_ingredient = Mock()
+            mock_ingredient.name = value[0] 
+            mock_ingredient.price = value[2]
+            mock_ingredient.type = ingredient_type  
+            mock_ingredients.append(mock_ingredient)
+
+        database.ingredients = mock_ingredients
+        ingredient = list(filter(lambda x: x.type == ingredient_type, database.ingredients))
         ingredient_names = [[ingredient.name, ingredient.type, ingredient.price] for ingredient in ingredient]
         assert ingredient_names == ingredient_value
+ 
 
     def test_available_buns(self,database):
+        mock_bun = Mock()
+        mock_bun.name = data.bun_name
+        mock_bun.price = data.bun_price
+        database.buns = [mock_bun]
         buns = database.available_buns()
         buns_list = [[bun.name, bun.price] for bun in buns]
-        assert buns_list == data.bun_list
+        assert buns_list == [[data.bun_name,data.bun_price]]
 
     def test_available_ingredients(self,database):
+        mock_ingredient = Mock()
+        mock_ingredient.name = data.ingredient_name
+        mock_ingredient.price = data.ingredient_price
+        mock_ingredient.type = data.ingredient_type
+        database.ingredients = [mock_ingredient]
         ingredients = database.available_ingredients()
-        ingredients_list = [[ingredient.name, ingredient.type, ingredient.price] for ingredient in ingredients]
-        assert ingredients_list == data.ingredients_list
+        ingredients_list = [[ingredient.type, ingredient.name, ingredient.price] for ingredient in ingredients]
+        assert ingredients_list == [[data.ingredient_type, data.ingredient_name,  data.ingredient_price]]
         
-    
-
